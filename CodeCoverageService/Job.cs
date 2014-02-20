4ac46b526e1823c50cd6c758f05e5195df1fb342
teddylong <teddy_lee11@hotmail.com>
2014-02-20 17:42:59 +0800

    checkl-in

---
+++ CodeCoverageService/Job.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCoverageService
+{
+    public class Job
+    {
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        private string startTime;
+
+        public string StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        private string endTime;
+
+        public string EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+
+        private string fileName;
+
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+
+        private string alive;
+
+        public string Alive
+        {
+            get { return alive; }
+            set { alive = value; }
+        }
+
+        private string machine;
+
+        public string Machine
+        {
+            get { return machine; }
+            set { machine = value; }
+        }
+    }
+}
---
+++ CodeCoverageService/LogService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace CodeCoverageService
+{
+    public class LogService
+    {
+        public static void WriteLog(string message, EventLogEntryType eventType)
+        {
+            if (!EventLog.SourceExists("CodeCoverageLog"))
+            {
+
+                EventLog.CreateEventSource("CodeCoverageLog", "CodeCoverageLog");
+                EventLog el = new EventLog();
+                el.Source = "CodeCoverageLog";
+                el.Log = "CodeCoverageLog";
+                el.WriteEntry(message, eventType);
+            }
+            else
+            {
+                EventLog myLog = new EventLog();
+                myLog.Source = "CodeCoverageLog";
+                myLog.Log = "CodeCoverageLog";
+                myLog.WriteEntry(message,eventType);
+            }
+        }
+    }
+}
