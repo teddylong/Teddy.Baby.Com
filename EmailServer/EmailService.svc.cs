924a68ca7307363fbe80c17133c2ce56f9cdb2cc
teddylong <teddy_lee11@hotmail.com>
2014-05-05 09:42:44 +0800

    Add one new Project

---
+++ EmailServer/EmailService.svc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+
+namespace EmailServer
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class EmailService : IService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public void SendMailWithNoAttach(string AddressList, string title, string body)
+        {
+            List<string> mailList = new List<string>();
+            string[] mails = AddressList.Split(new Char[] { ';' });
+
+
+            foreach (string mail in mails)
+            {
+                mailList.Add(mail);
+            }
+
+            List<string> fileList = new List<string>();
+            Mail.SendToEmail("appmail.sh.ctriptravel.com", 000, mailList, title, body, fileList);
+        }
+        public void SendMail(string AddressList, string title, string body, string fileName)
+        {
+            List<string> mailList = new List<string>();
+            string[] mails = AddressList.Split(new Char[] { ';' });
+            foreach (string mail in mails)
+            {
+                mailList.Add(mail);
+            }
+            
+            List<string> fileList = new List<string>();
+            string[] files = fileName.Split(';');
+            foreach (string file in files)
+            {
+                fileList.Add(file);
+            }
+            Mail.SendToEmail("appmail.sh.ctriptravel.com", 000, mailList, title, body, fileList);
+        }
+        public void SendMailWithHtml(string AddressList, string title, string body)
+        {
+            List<string> mailList = new List<string>();
+            string[] mails = AddressList.Split(new Char[] { ';' });
+
+            title = HttpUtility.HtmlDecode(title);
+            body = HttpUtility.HtmlDecode(body);
+
+            foreach (string mail in mails)
+            {
+                mailList.Add(mail);
+            }
+
+            List<string> fileList = new List<string>();
+            Mail.SendToEmail("appmail.sh.ctriptravel.com", 000, mailList, title, body, fileList);
+        }
+    }
+}
--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Net;
+using System.IO;
+using System.Web;
+using System.Collections;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
 using System.Threading;
-using Microsoft.VisualStudio.Coverage.Analysis;
 
 namespace TestingConsole
 {
@@ -11,10 +17,55 @@ namespace TestingConsole
     {
         static void Main(string[] args)
         {
-            CoverageInfo ci = CoverageInfo.CreateFromFile(@"d:\Users\yqlong\Desktop\output.coverage");
-            CoverageDS data = ci.BuildDataSet(null);
-            string outputName = DateTime.Now.ToString("yyyyMMddHHmm") + ".xml";
-            data.ExportXml(outputName);
+            Send("autotest@ctrip.com", "Testing", "This Email comes from our server ---> 192.168.43.202");
+        }
+
+        private static void SendMailNoAttach(string postData)
+        {
+            try
+            {
+                byte[] buffer = Encoding.GetEncoding("utf-8").GetBytes(postData);
+                string url = @"http://192.168.43.202:8888/SendMail.asmx";
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                req.Method = "POST";
+                req.Host = "192.168.81.123";
+                req.ContentType = "application/soap+xml; charset=utf-8";
+                //application/x-www-form-urlencoded
+                req.ContentLength = buffer.Length;
+                Console.WriteLine(req.ContentLength);
+                req.Headers.Add("SOAPAction: \"http://tempuri.org/SendMailWithHtml\"");
+                req.Timeout = 5000;
+
+                Stream reqst = req.GetRequestStream();
+                reqst.Write(buffer, 0, buffer.Length);
+                reqst.Flush();
+                HttpWebResponse response = req.GetResponse() as HttpWebResponse;
+                response.Close();
+                reqst.Close();
+
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public static void Send(string receiver, string title, string body)
+        {
+
+            title = HttpUtility.HtmlEncode(title);
+            body = HttpUtility.HtmlEncode(body);
+            string soaphdr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">";
+            string template = "<soap:Body><SendMailWithHtml xmlns=\"http://tempuri.org/\"><AddressList xsi:type=\"xsd:string\">{0}</AddressList><title xsi:type=\"xsd:string\">{1}</title><body xsi:type=\"xsd:string\">{2}</body></SendMailWithHtml></soap:Body>";
+
+
+            string postData = string.Format(template, receiver, title, body);
+            postData = soaphdr + postData + "</soap:Envelope>";
+            SendMailNoAttach(postData);
+            Console.WriteLine("Sent..." + DateTime.Now);
+
         }
     }
 }
